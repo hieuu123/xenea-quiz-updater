29939c39b34a9b71c19cec5a5f260889a152d748
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-10-07 12:23:23 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = "AI Picks"   # ⚠️ username login thật (không phải Display Name)
 WP_APP_PASSWORD = "BZjF 3sMe HQgG 041j y079 SaFQ"  # giữ nguyên format
-POST_ID = 294911  # ID bài muốn update
-TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer – October 7, 2025"
-CHECK_ANSWER = "A) Scroll."
+POST_ID = 295141  # ID bài muốn update
+TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer – October 8, 2025"
+CHECK_ANSWER = "B) Superchain."
 
 # ================ SCRAPE SITE 1 ================
 def scrape_quiz_site1():
@@ -29,7 +29,7 @@ def scrape_quiz_site1():
 
 # ================ SCRAPE SITE 2 ================
 def scrape_quiz_site2():
-    url = "https://www.quiknotes.in/xenea-wallet-daily-quiz-answer-7-october-2025/"
+    url = "https://www.quiknotes.in/xenea-wallet-daily-quiz-answer-8-october-2025/"
     print(f"[+] Scraping quiz from {url}")
     r = requests.get(url, timeout=15, headers={"User-Agent": "Mozilla/5.0"})
     r.raise_for_status()
