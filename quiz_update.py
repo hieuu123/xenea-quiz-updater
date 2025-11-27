5ddee2a30653c8c3158e7f592bdcd1d2c9651259
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-11-27 08:56:07 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -8,12 +8,12 @@ WP_USERNAME = "AI Picks"
 WP_APP_PASSWORD = "BZjF 3sMe HQgG 041j y079 SaFQ"
 POST_ID = 304794
 
-TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer for November 26, 2025"
-CHECK_ANSWER = "D) Verify source authenticity."
+TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer for November 27, 2025"
+CHECK_ANSWER = "c) From official vendor only"
 
 # ngày find & replace
-OLD_DATE = "November 26"
-NEW_DATE = "November 27"
+OLD_DATE = "November 27"
+NEW_DATE = "November 28"
 
 
 # ================ SCRAPE SITE 1 ================
