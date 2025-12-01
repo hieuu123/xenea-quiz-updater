c2f034fec985561c97609e3cc17621c2510122b7
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-01 13:52:08 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -8,12 +8,12 @@ WP_USERNAME = "AI Picks"
 WP_APP_PASSWORD = "BZjF 3sMe HQgG 041j y079 SaFQ"
 POST_ID = 304794
 
-TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer for November 30, 2025"
-CHECK_ANSWER = "A) Verify full address."
+TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer for December 1, 2025"
+CHECK_ANSWER = "A) Zero-value transfer."
 
 # ngày find & replace
-OLD_DATE = "November 30"
-NEW_DATE = "December 1"
+OLD_DATE = "December 1"
+NEW_DATE = "December 2"
 
 
 # ================ SCRAPE SITE 1 ================
@@ -36,7 +36,7 @@ def scrape_quiz_site1():
 
 # ================ SCRAPE SITE 2 ================
 def scrape_quiz_site2():
-    url = "https://www.quiknotes.in/xenea-wallet-daily-quiz-answer-29-november-2025/"
+    url = "https://www.quiknotes.in/xenea-wallet-daily-quiz-answer-2-december-2025/"
     print(f"[+] Scraping quiz from {url}")
     r = requests.get(url, timeout=15, headers={"User-Agent": "Mozilla/5.0"})
     r.raise_for_status()
