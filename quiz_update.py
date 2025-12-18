639f3de8b89076fff96fa0cc2186b624460c603e
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-12-18 10:51:02 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # ================= CONFIG =================
-WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
+WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
 POST_ID = 309434  # ID bài muốn update
