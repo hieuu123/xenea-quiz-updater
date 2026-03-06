1d1a4e75cf3afa3fe1a1049a35f382bc502b0adc
xialing267-droid <xialing267@gmail.com>
2026-03-06 08:37:24 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -8,9 +8,9 @@ from bs4 import BeautifulSoup
 WP_URL = "https://blog.mexc.fm/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 321997  # ID bài muốn update
+POST_ID = 322241  # ID bài muốn update
 TARGET_H2_TEXT = "Xenea Wallet Daily Quiz Today’s Answer - December 19, 2025"
-CHECK_ANSWER = "C) Decentralized AI network."
+CHECK_ANSWER = "C) Unchangeable data."
 
 # ================ SCRAPE SITE 1 ================
 def scrape_quiz_site1():
