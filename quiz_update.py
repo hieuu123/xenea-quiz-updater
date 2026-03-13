14146427aec2384adfb4718fd74cb93eccf29133
xialing267-droid <xialing267@gmail.com>
2026-03-13 08:16:00 +0700

    Update quiz_update.py

--- quiz_update.py
+++ quiz_update.py
@@ -31,7 +31,7 @@ def scrape_quiz_site1():
 
 # ================ SCRAPE SITE 2 ================
 def scrape_quiz_site2():
-    url = "https://www.quiknotes.in/xenea-wallet-daily-quiz-answer-13-march-2026/"
+    url = "https://www.quiknotes.in/xenea-wallet-daily-quiz-answer-14-march-2026/"
     print(f"[+] Scraping quiz from {url}")
     r = requests.get(url, timeout=15, headers={"User-Agent": "Mozilla/5.0"})
     r.raise_for_status()
